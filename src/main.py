266af1a30838daa43eef0ae5b90b3fb3ddf6f1b9
PriyaChandra7 <priyachandra1728@gmail.com>
2026-07-01 02:51:39 +0530

    Finalized including remove virtual env from repository

--- src/main.py
+++ src/main.py
@@ -40,7 +40,6 @@ def get_arguments():
 def main():
 
   args = get_arguments()
-  # Step 1: Read input files
   csv_data = read_csv(args.csv)
   resume_text = read_resume(args.resume)
 
@@ -48,42 +47,31 @@ def main():
     print("Resume could not be loaded. Exiting pipeline.")
     return
 
-  # Step 2: Extract fields
   resume_profile = extract_resume_fields(resume_text)
 
-  # Step 3: Normalize
+
   normalized_profile = normalize_profile(resume_profile)
 
-  # Step 4: Merge
   candidate_profile = merge_profiles(csv_data, normalized_profile)
 
-  # Step 5: Calculate confidence
   candidate_profile = calculate_confidence(
       candidate_profile,
       csv_data,
       normalized_profile
   )
-
-  # Step 6: Load configuration
+  
   config = load_config(args.config)
-
-  # Step 7: Project output
   final_output = project_profile(candidate_profile, config)
 
   validate_profile(final_output)
-
-  # Step 8: Create output folder if it doesn't exist
   os.makedirs("../output", exist_ok=True)
 
-  # Step 9: Save Canonical Profile
   with open("../output/canonical_profile.json", "w") as file:
       json.dump(candidate_profile, file, indent=4)
 
-  # Step 10: Save Final Output
   with open("../output/final_output.json", "w") as file:
       json.dump(final_output, file, indent=4)
-
-  print("\n✅ Pipeline executed successfully!")
+  print("\n Pipeline executed successfully!")
   print("Canonical profile saved in output/canonical_profile.json")
   print("Final projected output saved in output/final_output.json")
 
